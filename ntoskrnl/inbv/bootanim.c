9bba15c0ce865c83d962b22ede98c8c4fdd8937d
Cheeseking <yacinebaya9@gmail.com>
2026-07-15 15:29:44 -0400

    Update boot messages for Lucid Box

--- ntoskrnl/inbv/bootanim.c
+++ ntoskrnl/inbv/bootanim.c
@@ -870,7 +870,7 @@ GetFamousQuote(VOID)
         "I have come here to chew bubble gum and kick ass,\r\n"
             "and I'm all out of bubble gum!",
         "That's the way the cookie crumbles ;-)",
-        "ReactOS is ready to be booted again ;-)",
+        "Lucid Box is ready to be booted again ;-)",
         "NOOOO!! DON'T HIT THE BUTTON! I wouldn't do it to you!",
         "Don't abandon your computer, he wouldn't do it to you.",
         "Oh, come on. I got a headache. Leave me alone, will ya?",
@@ -906,7 +906,8 @@ GetFamousQuote(VOID)
             "reaper! - Death in Bill and Ted's Bogus Journey",
         "Final Surrender",
         "If you see this screen...",
-        "From ReactOS with Love",
+        "From Lucid Box with Love",
+        "Godspeed, you magnificent bastard.",
         // "<Place your Ad here>"
     };
 
